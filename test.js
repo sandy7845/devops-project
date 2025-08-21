439ab5ea576e91a932053ecb6595def029ed9f00
sandy7845 <27sandy3@gmail.com>
2025-08-21 00:27:17 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+const greet = require('./app');
+
+if(greet()==="Hello from DevOps!"){
+    consider.log(" Test passed");
+    Process.exit(0);
+}else {
+    console.log(" Test failed");
+    process.exit
+}
